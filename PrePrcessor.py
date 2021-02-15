98b6d3515e9f094d56de542ac8de00e9bb3dfb2d
cakeforcat <cakeforcat@gmail.com>
2021-02-15 14:16:36 +0100

    Implemented reading and pre-processing of .csv files

---
+++ PrePrcessor.py
@@ -0,0 +1,51 @@
+from readcsv import DataReader
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def detect_event(diff, max_diff, min_diff):
+    if diff > (0.1*max_diff) or diff < (0.1*min_diff):
+        return 0
+    else:
+        return None
+
+
+class PreProcessor(DataReader):
+    def __init__(self, file_name):
+        DataReader.__init__(self, file_name)
+
+    def read_and_process(self, data_series, data_range, chunk_size):
+        df = None
+        df_diffed = None
+        with self.read_in_chunks(data_series, data_range, chunk_size) as reader:
+            continuity_cache = None
+            processed_rows = 0
+            for chunk in reader:
+                chunk.set_index("Time", drop=True, inplace=True)
+                chunk = pd.concat([continuity_cache, chunk])
+                continuity_cache = chunk.tail(1)
+                chunk_diffed = chunk.diff(periods=1)
+                chunk.drop(chunk.index[0], inplace=True)
+                chunk_diffed.drop(chunk.index[0], inplace=True)
+                df_diffed = pd.concat([df_diffed, chunk_diffed])
+                df = pd.concat([df, chunk])
+                processed_rows = processed_rows + chunk_size
+                percentage = (processed_rows*100)/(data_range[1]-data_range[0])
+                print("processed: {:.2f}%".format(percentage))
+        return df, df_diffed
+
+
+def example():
+    processor = PreProcessor("Signature Database dataset.csv")
+    df, df_diffed = processor.read_and_process(['Kettle'], [100, 30000], 1000)
+    print(df_diffed)
+    print(df)
+    print(df.max()['Kettle'], df.min()['Kettle'])
+    df_diffed["Event"] = df_diffed.apply(lambda x: detect_event(x['Kettle'], df_diffed.max()['Kettle'],
+                                                                df_diffed.min()['Kettle']), axis=1)
+    ax = df.plot()
+    df_diffed.reset_index().plot( x="Time", y='Event', kind='scatter', ax=ax, c='orange', s=200)
+    plt.gcf().autofmt_xdate(rotation=20)
+    plt.show()
+
+example()
--- readcsv.py
+++ readcsv.py
@@ -1,14 +1,13 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
 class DataReader:
     """
-    # file_name is a str containing the file path for the csv file to open
-    # data_series is a list containing the names of the columns to be included. for large files stick to around 2.
-    # Don't include the "Time" column as it's internally included as an index
-    # data_range is a list containing [start, end] numbers of the range of rows to be read. start and end are included
-    # omitted, the whole range is read.
+    file_name is a str containing the file path for the csv file to open
+    data_series is a list containing the names of the columns to be included. for large files stick to around 2.
+    Don't include the "Time" column as it's internally included as an index
+    data_range is a list containing [start, end] numbers of the range of rows to be read. start and end are included
+    omitted, the whole range is read.
     """
 
     def __init__(self, file_name):
@@ -25,14 +24,13 @@ class DataReader:
             return pd.read_csv(self.file_name, usecols=data_series, index_col="Time",
                                parse_dates=True, nrows=number_of_rows, skiprows=skipped_range)
 
-
-# example use
-reader = DataReader("Signature Database dataset.csv")
-df = reader.read_to_dataframe(["Computer", "Kettle"], [30300, 30400])
-# plot the example
-plt.close("all")
-plt.figure()
-df.plot()
-plt.legend(loc='best')
-plt.show()
-print(df)
+    def read_in_chunks(self, data_series, data_range=None, chunk_size=1000):
+        data_series.append("Time")
+        if data_range is None:
+            return pd.read_csv(self.file_name, usecols=data_series,
+                               parse_dates=["Time"], chunksize=chunk_size)
+        else:
+            number_of_rows = data_range[1] - data_range[0]
+            skipped_range = range(1, data_range[0] - 1)
+            return pd.read_csv(self.file_name, usecols=data_series, parse_dates=["Time"], nrows=number_of_rows,
+                               skiprows=skipped_range, chunksize=chunk_size)
